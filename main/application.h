e5355cc594a69471aa84668ac886b2216d2e2a4a
奉有余 <fncong@outlook.com>
2025-04-16 05:17:38 +0800

    fix

--- main/application.h
+++ main/application.h
@@ -32,7 +32,8 @@
 #define AUDIO_OUTPUT_READY_EVENT (1 << 2)
 #define CHECK_NEW_VERSION_DONE_EVENT (1 << 3)
 
-enum DeviceState {
+enum DeviceState
+{
     kDeviceStateUnknown,
     kDeviceStateStarting,
     kDeviceStateWifiConfiguring,
@@ -47,22 +48,24 @@ enum DeviceState {
 
 #define OPUS_FRAME_DURATION_MS 60
 
-class Application {
+class Application
+{
 public:
-    static Application& GetInstance() {
+    static Application &GetInstance()
+    {
         static Application instance;
         return instance;
     }
     // 删除拷贝构造函数和赋值运算符
-    Application(const Application&) = delete;
-    Application& operator=(const Application&) = delete;
+    Application(const Application &) = delete;
+    Application &operator=(const Application &) = delete;
 
     void Start();
     DeviceState GetDeviceState() const { return device_state_; }
     bool IsVoiceDetected() const { return voice_detected_; }
     void Schedule(std::function<void()> callback);
     void SetDeviceState(DeviceState state);
-    void Alert(const char* status, const char* message, const char* emotion = "", const std::string_view& sound = "");
+    void Alert(const char *status, const char *message, const char *emotion = "", const std::string_view &sound = "");
     void DismissAlert();
     void AbortSpeaking(AbortReason reason);
     void ToggleChatState();
@@ -70,8 +73,8 @@ public:
     void StopListening();
     void UpdateIotStates();
     void Reboot();
-    void WakeWordInvoke(const std::string& wake_word);
-    void PlaySound(const std::string_view& sound);
+    void WakeWordInvoke(const std::string &wake_word);
+    void PlaySound(const std::string_view &sound);
     bool CanEnterSleepMode();
 
 private:
@@ -105,7 +108,7 @@ private:
 
     // Audio encode / decode
     TaskHandle_t audio_loop_task_handle_ = nullptr;
-    BackgroundTask* background_task_ = nullptr;
+    BackgroundTask *background_task_ = nullptr;
     std::chrono::steady_clock::time_point last_output_time_;
     std::list<std::vector<uint8_t>> audio_decode_queue_;
     std::condition_variable audio_decode_cv_;
@@ -120,7 +123,7 @@ private:
     void MainEventLoop();
     void OnAudioInput();
     void OnAudioOutput();
-    void ReadAudio(std::vector<int16_t>& data, int sample_rate, int samples);
+    void ReadAudio(std::vector<int16_t> &data, int sample_rate, int samples);
     void ResetDecoder();
     void SetDecodeSampleRate(int sample_rate, int frame_duration);
     void CheckNewVersion();
@@ -128,6 +131,7 @@ private:
     void OnClockTimer();
     void SetListeningMode(ListeningMode mode);
     void AudioLoop();
+    void MiHome();
 };
 
 #endif // _APPLICATION_H_
